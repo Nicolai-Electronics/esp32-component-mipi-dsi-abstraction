b29bc5e48df5f4053bbb272865139aeec0d7a2f5
Renze Nicolai <renze@rnplus.nl>
2025-05-10 14:48:01 +0200

    Fix return value type for ili9881

--- include/dsi_panel_espressif_ili9881c.h
+++ include/dsi_panel_espressif_ili9881c.h
@@ -5,5 +5,5 @@
 #include "esp_lcd_mipi_dsi.h"
 
 esp_lcd_panel_handle_t ili9881c_get_panel(void);
-void ili9881c_initialize(gpio_num_t reset_pin);
-void ili9881c_get_parameters(size_t* h_res, size_t* v_res, lcd_color_rgb_pixel_format_t* color_fmt);
+esp_err_t ili9881c_initialize(gpio_num_t reset_pin);
+esp_err_t ili9881c_get_parameters(size_t* h_res, size_t* v_res, lcd_color_rgb_pixel_format_t* color_fmt);
